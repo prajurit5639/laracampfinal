1f3821af5633ecf31e67ea564356e52dce98dab5
prajurit5639 <106043804+prajurit5639@users.noreply.github.com>
2022-06-05 03:39:48 +0800

    first commit

---
+++ routes/web.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\User\CheckoutController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+})->name('welcome');
+
+
+Route::get('checkout/success}',[CheckoutController::class,'success'])->name('checkout.success');
+
+Route::get('checkout/{camp:slug}',[CheckoutController::class,'create'])->name('checkout.create');
+Route::post('checkout/{camp}',[CheckoutController::class,'store'])->name('checkout.store');
+
+Route::get('sign-in-google',[UserController::class,'google'])->name('user.login.google');
+
+Route::get('auth/google/callback', [UserController::class,'handleProviderCallback'])->name('user.google.callback');
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+require __DIR__.'/auth.php';
